f2ed2d86f0ddff6e62813767a03bffe3fbdf80e4
mitriifd <idimka9@gmail.com>
2022-02-21 18:17:24 +0500

    Практика с жизненным циклом

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,13 +3,13 @@ class MarvelService {
     // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
     _apiKey = 'apikey=197a219df3e76758567c5c06ac0d6580';
 
-   getResource = async (url) => {
+    getResource = async (url) => {
         let res = await fetch(url);
-    
+
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-    
+
         return await res.json();
     }
 
@@ -25,11 +25,13 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
